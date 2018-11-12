4ea1c183fe0f38076fa99ecf653f8fa7b452fe15
sreeise <reeisesean@gmail.com>
2018-11-11 21:12:20 -0500

    Remove useless color setting for dark theme

--- src/react-wheel/src/components/theme/dark.jsx
+++ src/react-wheel/src/components/theme/dark.jsx
@@ -3,7 +3,6 @@ import {createMuiTheme} from "@material-ui/core";
 const darkTheme = createMuiTheme({
   palette: {
     type: "dark",
-    secondary: {main: "#11cb5f"}
   },
   typography: {
     useNextVariants: true
